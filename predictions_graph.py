48b4a2c10fc7b30c89d1f98b8e75e5866fe31579
ksjost <104531375+ksjost@users.noreply.github.com>
2023-07-19 14:31:25 -1000

    Creates graph from pickled data

---
+++ predictions_graph.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import pickle
+
+def main():
+    """Main Program"""
+
+    # Load the pickled data fron lable_counter.pkl
+    with open("label_counter.pkl", "rb") as file:
+        label_counter = pickle.load(file)
+    
+    labels = list(label_counter.keys())
+    scores = list(label_counter.values())
+
+    plt.barh(labels, scores)
+    plt.xlabel("Rounded Probability Sum")
+    plt.ylabel("Predicted Label")
+    plt.title("Bar Graph of Summed Predicted Label Probabilities")
+    plt.savefig("predictions_graph.png")
+
+if __name__ == "__main__":
+    main()
