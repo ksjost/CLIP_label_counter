95aa91ff2526546e7adb45efc2bf65aad784944b
ksjost <104531375+ksjost@users.noreply.github.com>
2023-07-19 14:29:54 -1000

    Main program

---
+++ label_counter.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Check if the correct number of arguments is provided 
+if [ $# -ne 1 ]; then 
+    echo "Usage: $0 <path_to_directory>"
+    exit 1
+fi
+
+# Get the directory path from the first argument 
+directory="$1"
+
+# Check if the provided directory exists
+if [ ! -d "$directory" ]; then 
+    echo "Error: Directory '$directory' does not exist."
+    exit 1
+fi
+
+# Run the python script with the directory as an argument to get label predictions
+echo "Running label_counter.py..."
+python label_counter.py "$directory"
+
+# Run the python script to plot predictions
+echo "Running predictions_graph.py..."
+python predictions_graph.py
+
+# Check if the python script successfully generated the plot file 
+if [ ! -f "predictions_graph.png" ]; then 
+    echo "Error: The python script did not generate the plot file."
+    exit 1
+fi 
+
+echo "Plot file 'predictions_graph.png' created and saved in the current directory."
+echo "Done!"
