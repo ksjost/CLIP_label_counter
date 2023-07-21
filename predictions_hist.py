a292be3e6540f41c997fb6d7a197db5859c8f9ff
ksjost <104531375+ksjost@users.noreply.github.com>
2023-07-21 09:18:55 -1000

    Creates histogram form similarity_dict.pkl

--- predictions_hist.py
+++ predictions_hist.py
@@ -1,9 +1,16 @@
 import matplotlib.pyplot as plt
 import pickle
 import os
+import yaml
 
 def main():
     """Main Program"""
+
+    # Load the YAML data from the file 
+    with open("word_labels.yaml", "r") as file: 
+        yaml_doc = yaml.safe_load(file)
+
+    probability = yaml_doc["probability cutoff"][0]
 
     # Load the pickled data from similarity_dict.pkl
     with open("similarity_dict.pkl", "rb") as file:
@@ -12,12 +19,13 @@ def main():
     os.makedirs("histograms", exist_ok=True)
     
     for word in similarity_dict.keys():
-        percents = [p for p in similarity_dict[word] if p >= 20]
+        probs = [p for p in similarity_dict[word] if p >= probability]
         plt.figure()
-        plt.hist(percents, bins=50)
-        plt.xlabel("Percent Similarity")
+        plt.hist(similarity_dict[word], bins=50)
+        plt.axvline(probability, color="r")
+        plt.xlabel("Similarity Probability")
         plt.ylabel("Frequency")
-        plt.title(f"{word} is a match for {len(percents)} files")
+        plt.title(f"probability({word} is similar) >= {probability} for {len(probs)} files")
         plt.savefig(f"histograms/{word}_hist.png")
     
 if __name__ == "__main__":
